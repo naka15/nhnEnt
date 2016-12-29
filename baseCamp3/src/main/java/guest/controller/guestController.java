69be9e48fe7321853f83955e0b519d71299a642d
naka15 <nakaa15@naver.com>
2016-12-29 15:24:39 +0900

    server start error 수정

---
+++ baseCamp3/src/main/java/guest/controller/guestController.java
@@ -0,0 +1,28 @@
+package guest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import guest.dto.testDTO;
+
+@Controller
+@RequestMapping("/guest/test/")
+public class guestController {
+ 
+    @RequestMapping(value= "/bodyTest", method = RequestMethod.GET)
+    public @ResponseBody testDTO sayHello(@RequestBody testDTO model) {
+    	
+        return model;
+    }
+ 
+    @RequestMapping(value="/helloagain.do", method = RequestMethod.GET)
+    public String sayHelloAgain(ModelMap model) {
+        model.addAttribute("greeting", "Hello World Again, from Spring 4 MVC");
+        return "welcome";
+    }
+ 
+}
---
+++ baseCamp3/src/main/java/guest/dto/testDTO.java
@@ -0,0 +1,14 @@
+package guest.dto;
+
+public class testDTO {
+	private String str;
+
+	public String getStr() {
+		return str;
+	}
+
+	public void setStr(String str) {
+		this.str = str;
+	}
+	
+}
